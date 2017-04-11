c02721952152b05d7e2ea53cc3ccffbe404e2ec5
Tomy Salminen <tomy.salminen@futurice.com>
2017-04-11 12:59:00 +0300

    Dont count vote scores for feed_items from banned users

--- src/core/team-core.js
+++ src/core/team-core.js
@@ -14,14 +14,16 @@ function getTeams(opts) {
         wilsons(
           SUM(CASE votes.value WHEN 1 THEN (1 - voter_biases.bias) ELSE null END)::numeric,
           SUM(CASE votes.value WHEN -1 THEN voter_biases.bias ELSE null END)::numeric
-        )
-        AS value,
+        ) AS value,
         users.team_id AS team_id
-      FROM feed_items
+      FROM (
+        SELECT feed_items.*
+        FROM feed_items
+        JOIN users ON users.id = feed_items.user_id AND NOT users.is_banned
+      ) feed_items
       JOIN users ON users.id = feed_items.user_id
       LEFT JOIN votes ON votes.feed_item_id = feed_items.id
       JOIN voter_biases ON voter_biases.user_id = votes.user_id AND voter_biases.team_id = users.team_id
-      WHERE NOT users.is_banned
       GROUP BY feed_items.id, users.team_id
     ) AS sub_query
     GROUP BY team_id)
@@ -30,26 +32,23 @@ function getTeams(opts) {
   const actionScoreSql = `
     (SELECT
       SUM(COALESCE(action_types.value, 0)) AS value,
-      teams.id AS team_id,
-      teams.name AS team_name,
-      teams.image_path AS image_path,
-      teams.city_id AS city_id
+      teams.id AS team_id
     FROM teams
     LEFT JOIN actions ON teams.id = actions.team_id ${isBanned ? '' : 'AND NOT actions.is_banned'}
     LEFT JOIN action_types ON actions.action_type_id = action_types.id
-    LEFT JOIN users ON users.id = actions.user_id
-    WHERE NOT users.is_banned
+    LEFT JOIN users ON users.id = actions.user_id AND NOT users.is_banned
     GROUP BY teams.id)
   `;
 
   let sqlString = `
     SELECT
-      actions_score.team_id AS id,
-      actions_score.team_name AS name,
-      actions_score.image_path AS image_path,
+      teams.id AS id,
+      teams.name AS name,
+      teams.image_path AS image_path,
       (COALESCE(actions_score.value, 0) + COALESCE(vote_score.value, 0))::int AS score,
-      actions_score.city_id AS city
-    FROM ${ actionScoreSql } AS actions_score
+      teams.city_id AS city
+    FROM teams
+    LEFT JOIN ${ actionScoreSql } actions_score ON actions_score.team_id = teams.id
     LEFT JOIN ${ voteScoreSql } vote_score ON vote_score.team_id = actions_score.team_id
   `;
 
@@ -57,7 +56,7 @@ function getTeams(opts) {
   let whereClauses = [];
 
   if (opts.city) {
-    whereClauses.push('actions_score.city_id = ?');
+    whereClauses.push('teams.city_id = ?');
     params.push(opts.city);
   }
 
