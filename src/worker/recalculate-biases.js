05a1418495a786ba55fc9f2768cb0a549f5ac5a9
Tomy Salminen <tomy.salminen@futurice.com>
2017-04-11 08:04:11 +0300

    Optimize upserting redundant rows

--- src/worker/recalculate-biases.js
+++ src/worker/recalculate-biases.js
@@ -1,11 +1,9 @@
 #!/usr/bin/env node
 import BPromise from 'bluebird';
-import _ from 'lodash';
 import * as biasCore from '../core/bias-core';
 const {knex} = require('../util/database').connect();
 const logger = require('../util/logger')(__filename);
 
-let teams;
 
 _updateBiases()
 .then(() => {
@@ -19,21 +17,18 @@ _updateBiases()
 
 function _updateBiases() {
   logger.info('Beginning bias update');
-
-  return knex('teams').select('id').then(results => {
-    teams = results;
-
-    return knex('users').select('id');
-  })
-  .then(users => BPromise.each(users, user =>
-    BPromise.each(teams, team =>
+  return knex('votes').select(['votes.user_id', 'users.team_id'])
+    .innerJoin('feed_items', 'feed_items.id', 'votes.feed_item_id')
+    .innerJoin('users', 'users.id', 'feed_items.user_id')
+    .groupBy(['votes.user_id', 'users.team_id'])
+    .orderBy('votes.user_id')
+    .then(rows => BPromise.each(rows, row =>
       biasCore.calculateBias({
-        userId: user.id,
-        teamId: team.id,
+        userId: row.user_id,
+        teamId: row.team_id,
       })
       .then(() => {
         logger.info('Row updated');
       })
-    )
-  ));
+    ));
 }
