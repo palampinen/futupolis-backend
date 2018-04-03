16827a310fc709213ffd135007a93d2cd044ba1a
Pasi Lampinen <plam@dyn-100-68.wlan.futurice.org>
2018-04-03 11:10:46 +0300

    comment image upload

--- src/core/comment-core.js
+++ src/core/comment-core.js
@@ -1,20 +1,57 @@
-const {knex} = require('../util/database').connect();
+import _ from 'lodash';
+import * as imageCore from './image-core';
+import { pathToUrl } from '../util/gcs';
+import * as imageHttp from '../http/image-http';
+import { decodeBase64Image } from '../util/base64';
+import moment from 'moment-timezone';
+const { knex } = require('../util/database').connect();
+const BPromise = require('bluebird');
+const uuidV1 = require('uuid/v1');
 
+function _makeCommentDbRow(action) {
+  const row = {
+    user_id: action.client.id,
+    feed_item_id: action.feedItemId,
+  };
 
-const newComment = (action) =>  knex('comments').insert({
-  user_id: action.client.id,
-  feed_item_id: action.feedItemId,
-  text: action.text,
-})
-.catch((err) => {
-  if (err.constraint === 'comments_feed_item_id_foreign') {
-    const error = new Error('No such feed item id');
-    error.status = 404;
-    throw error;
+  if (action.text) {
+    row.text = action.text;
   }
 
-  throw err;
-});
+  if (action.imagePath) {
+    row.image_path = action.imagePath;
+  }
+
+  return row;
+}
+
+function newComment(action) {
+  // image if needed
+  const filePath = `${imageCore.targetFolder}/${uuidV1()}`;
+
+  const saveImage = action.imageData
+    ? imageHttp.uploadImage(filePath, decodeBase64Image(action.imageData))
+    : BPromise.resolve(null);
+
+  return saveImage.then(imgPath => {
+    const imgUpdate = imgPath ? { imagePath: imgPath.imageName } : {};
+    const commentUpdate = _.merge({}, action, imgUpdate);
+    const dbRow = _makeCommentDbRow(commentUpdate);
+
+    knex('comments')
+      .insert(dbRow)
+      .catch(err => {
+        if (err.constraint === 'comments_feed_item_id_foreign') {
+          const error = new Error('No such feed item id');
+          error.status = 404;
+          throw error;
+        }
+
+        throw err;
+      });
+  });
+}
+
 function deleteComment(id, opts) {
   opts = opts || {};
 
@@ -34,6 +71,4 @@ function deleteComment(id, opts) {
     });
 }
 
-export {
-  newComment,
-};
+export { newComment, deleteComment };
