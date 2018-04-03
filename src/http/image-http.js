d1523c971716427bad106fa7e486c2cf3f146cce
Pasi Lampinen <plam@dyn-100-68.wlan.futurice.org>
2018-04-03 15:15:19 +0300

    export uploadImage

--- src/http/image-http.js
+++ src/http/image-http.js
@@ -5,11 +5,11 @@
 
 import * as gcs from '../util/gcs';
 import * as actionCore from '../core/action-core';
-import {createJsonRoute, throwStatus} from '../util/express';
+import { createJsonRoute, throwStatus } from '../util/express';
 import * as userCore from '../core/user-core';
 import * as imageCore from '../core/image-core';
-import {assert} from '../validation';
-import {decodeBase64Image} from '../util/base64';
+import { assert } from '../validation';
+import { decodeBase64Image } from '../util/base64';
 import { processImage } from '../util/image-processor';
 
 const logger = require('../util/logger')(__filename);
@@ -18,25 +18,23 @@ const uuidV1 = require('uuid/v1');
 const ALLOWED_MIME_TYPES = new Set(['image/jpeg', 'image/gif', 'image/png']);
 
 function getAndValidateActionType(typeName) {
-  return actionCore.getActionType(typeName)
-    .then(type => {
-      if (type === null) {
-        throwStatus(400, `Action type ${ typeName } does not exist`);
-      }
+  return actionCore.getActionType(typeName).then(type => {
+    if (type === null) {
+      throwStatus(400, `Action type ${typeName} does not exist`);
+    }
 
-      return type;
-    });
+    return type;
+  });
 }
 
 function getAndValidateUser(uuid) {
-  return userCore.findByUuid(uuid)
-    .then(user => {
-      if (user === null) {
-        throwStatus(400, `User with uuid ${ uuid } does not exist`);
-      }
+  return userCore.findByUuid(uuid).then(user => {
+    if (user === null) {
+      throwStatus(400, `User with uuid ${uuid} does not exist`);
+    }
 
-      return user;
-    });
+    return user;
+  });
 }
 
 function uploadImage(imageName, imageFile, imageOpts) {
@@ -52,31 +50,31 @@ function uploadImage(imageName, imageFile, imageOpts) {
       }
     })
     .then(buffer => gcs.uploadImageBuffer(imageName, buffer));
-};
+}
 
 function validateMimeType(mimetype) {
   if (ALLOWED_MIME_TYPES.has(mimetype)) {
     return Promise.resolve();
   } else {
-    throw new Error(`Unsupported file type ${ mimetype } uploaded`);
+    throw new Error(`Unsupported file type ${mimetype} uploaded`);
   }
 }
 
-
-
 const getImage = createJsonRoute(function(req, res) {
-  const params = assert({
-    imageId: req.params.id
-  }, 'imageParams');
+  const params = assert(
+    {
+      imageId: req.params.id,
+    },
+    'imageParams'
+  );
 
-  return imageCore.getImageById(params.imageId)
-    .then(image => {
-      if (!image) {
-        throwStatus(404, 'No such image id');
-      } else {
-        return image;
-      }
-    });
+  return imageCore.getImageById(params.imageId).then(image => {
+    if (!image) {
+      throwStatus(404, 'No such image id');
+    } else {
+      return image;
+    }
+  });
 });
 
 function postImage(req, res) {
@@ -96,24 +94,23 @@ function postImage(req, res) {
     .then(user => {
       inputData.user = user;
 
-      const fileName = `${ imageCore.targetFolder }/${ uuidV1() }`;
+      const fileName = `${imageCore.targetFolder}/${uuidV1()}`;
       return uploadImage(fileName, image, imageOpts);
     })
     .then(uploadedImage => {
-      return actionCore.createAction({
-        ip:        req.ip,
-        isBanned:  req.client.isBanned,
-        type:      action.type,
-        user:      action.user,
-        location:  action.location,
-        imagePath: uploadedImage.imageName,
-        city:      action.city,
-        client:    req.client
-      }).then(rowsInserted => undefined);
+      return actionCore
+        .createAction({
+          ip: req.ip,
+          isBanned: req.client.isBanned,
+          type: action.type,
+          user: action.user,
+          location: action.location,
+          imagePath: uploadedImage.imageName,
+          city: action.city,
+          client: req.client,
+        })
+        .then(rowsInserted => undefined);
     });
-};
+}
 
-export {
-  getImage,
-  postImage
-};
+export { getImage, uploadImage, postImage };
